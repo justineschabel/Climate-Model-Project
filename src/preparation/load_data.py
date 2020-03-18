52e80028c1c3d36b43b34ee50454e2e30e9d7451
jschabel <justine.schabel@gmail.com>
2020-03-18 11:52:40 -0700

    Completed data loading and transform

--- src/preparation/load_data.py
+++ src/preparation/load_data.py
@@ -1,30 +1,27 @@
-# Source: https://machinelearningmastery.com/load-machine-learning-data-python/
-import csv
-import numpy
-
-filename = '../../data/raw/greenhouse_gas_inventory_data_data.csv'
-raw_data = open(filename, 'rt')
-reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
-x = list(reader)
-data = numpy.array(x)
-
-"""
-Should I convert:
-    Convert first column (countries) to one-hot? 
-    Convert second (year) and third (value in kilotons equivalent CO2) column to float 
-    Convert fourth column (category) to one-hot or maybe remove it ?
-If I want to predict: 
-    Global Average temperature I need:
-        (1)Predict the output of each country by year X
-        Encorporate growth somehow? - India is going to industrialize and will produce more
-        wheras the US and other developed countries should be decreasing their emissions
-        Find relationship between GHG emissions and temperature and use predicted amount of emissions to estimate
-In order to decide what type of model I should try to build, I need to visualize this data
-    Perhaps [country, year (x axis), GHG (y axis)] 
-"""
+import pandas as pd
+import numpy as np
 
 
 
-# Skip header?
-# Is this the right orientation?
-# print(data[1:][1]) Why isn't this the first to last row and first column ?
+def read_data(filename):
+    """ Read data from CSV.
+  
+    Keyword arguments:
+    filename -- path to the csv
+    
+    Return a numpy array of size (number_of_countries, number of entries per country, 3)
+    """
+    names = ['country_or_area', 'year', 'value', 'category']
+    data = pd.read_csv(filename, names=names, skiprows=1)# , index_col='country_or_area')
+
+    # source: https://stackoverflow.com/questions/52621497/pandas-group-by-column-and-transform-the-data-to-numpy-array
+    group_by_country = data.groupby('country_or_area').cumcount()  
+    multi_indexed = pd.MultiIndex.from_product([data['country_or_area'].unique(), group_by_country.unique()])
+    to_list = (data.set_index(['country_or_area',group_by_country])
+       .reindex(mux, fill_value=0)
+       .groupby(level=0)['year','value', 'category']
+       .apply(lambda x: x.values.tolist())
+       .tolist()
+    )
+
+    return np.array(to_list)
